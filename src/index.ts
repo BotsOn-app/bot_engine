8b27837506e673eb7ac2411dc49ba003349059d9
5CYTH3 <ScytheD@outlook.fr>
2023-05-28 16:51:13 +0200

    This is terrific. I can't test and it scares me

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,54 @@
-import { Client } from 'discord.js'
+import {
+	Client,
+	CommandInteraction,
+	IntentsBitField,
+	REST,
+	Routes,
+	SlashCommandBuilder,
+} from 'discord.js'
+import { Command, CommandBuilder } from './commands/commandBuilder'
+
+type BotSpecs = { token: string; CLIENT_ID: string; GUILD_ID: string }
 
 export class Bot {
 	public client: Client
-	constructor() {
+	private specs: BotSpecs
+
+	private commands: {
+		data: SlashCommandBuilder
+		execute(interaction: CommandInteraction): Promise<void>
+	}[] = []
+
+	constructor(botSpecs: BotSpecs) {
+		this.specs = botSpecs
 		this.client = new Client({
-			intents: [],
+			intents: [
+				IntentsBitField.Flags.Guilds,
+				IntentsBitField.Flags.GuildMembers,
+				IntentsBitField.Flags.GuildMessages,
+				IntentsBitField.Flags.MessageContent,
+			],
 		})
 	}
 
-	public login(token: string) {
-		this.client.login(token)
+	public login() {
+		this.client.login(this.specs.token)
+	}
+
+	public addCommand(command: Command) {
+		this.commands.push(command.convert())
+	}
+
+	public async registerCommands() {
+		let rest = new REST({ version: '10' }).setToken(this.specs.token)
+		await rest.put(
+			Routes.applicationGuildCommands(
+				this.specs.CLIENT_ID,
+				this.specs.GUILD_ID
+			),
+			{
+				body: this.commands,
+			}
+		)
 	}
 }
